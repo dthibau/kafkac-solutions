87fd5e9a99f7491199804013f64dabaaec07d355
David THIBAU <david.thibau@windows.com>
2024-09-05 13:29:53 +0200

    Stream arrondit la Position

---
+++ Solutions/KafkaStream/Program.cs
@@ -0,0 +1,51 @@
+﻿using Streamiz.Kafka.Net.SerDes;
+using Streamiz.Kafka.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Streamiz.Kafka.Net.SchemaRegistry.SerDes.Avro;
+using Confluent.Kafka;
+using Streamiz.Kafka.Net.Stream;
+using model;
+
+namespace PositionStream
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var config = new StreamConfig<Int64SerDes, SchemaAvroSerDes<Coursier>>();
+            config.ApplicationId = "position-stream";
+            config.BootstrapServers = "localhost:19092";
+            config.SchemaRegistryUrl = "http://localhost:8081"; // URL of your Schema Registry
+            config.AutoOffsetReset = AutoOffsetReset.Earliest;
+
+            StreamBuilder builder = new StreamBuilder();
+
+            builder.Stream<long, Coursier>("position-avro")
+                .MapValues(c =>
+                {
+                Position position = (Position)c.position;
+                position.latitude = Math.Round(position.latitude, 1);
+                position.longitude = Math.Round(position.longitude, 1);
+                    return c;
+                })
+                .To("position-output");
+
+            Topology t = builder.Build();
+            KafkaStream stream = new KafkaStream(t, config);
+
+            // Subscribe CTRL + C to quit stream application
+            Console.CancelKeyPress += (o, e) =>
+            {
+                stream.Dispose();
+            };
+
+            // Start stream instance with cancellable token
+            await stream.StartAsync();
+
+        }
+    }
+}
